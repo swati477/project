49225ade51e2ef8c7804a037655bd00109197ea2
swati477 <swati.shinde@mobavenue.com>
2025-11-25 16:28:32 +0530

    Update etl_pipeline.py

--- etl/dags/etl_pipeline.py
+++ etl/dags/etl_pipeline.py
@@ -1,11 +1,14 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+import os
 
-# DAG setup
+# Define base project path (make it relative to Airflow DAG folder)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 with DAG(
     dag_id="etl_pipeline",
-    description="Simple ETL pipeline: extract → transform → load",
+    description="ETL pipeline: extract → transform → load",
     start_date=datetime(2024, 1, 1),
     schedule_interval="@daily",
     catchup=False
@@ -14,20 +17,20 @@ with DAG(
     # Step 1: Extract
     extract = BashOperator(
         task_id="extract",
-        bash_command="python /project/etl/extract.py"
+        bash_command=f"python {os.path.join(BASE_DIR, '../etl/extract.py')}"
     )
 
     # Step 2: Transform
     transform = BashOperator(
         task_id="transform",
-        bash_command="python /project/etl/transform.py"
+        bash_command=f"python {os.path.join(BASE_DIR, '../etl/transform.py')}"
     )
 
     # Step 3: Load into SQLite
     load = BashOperator(
         task_id="load",
-        bash_command="python /project/etl/load.py"
+        bash_command=f"python {os.path.join(BASE_DIR, '../etl/load.py')}"
     )
 
-    # Pipeline sequence
+    # Define pipeline sequence
     extract >> transform >> load
